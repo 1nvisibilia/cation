658448a8db1b0a6e11819fdedfc79091e3c42106
Jiadi Tao <jiadi.tao@uwaterloo.ca>
2022-12-29 23:44:59 -0500

    optimize code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,17 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                // for more performance code splitting
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
     }
 });
